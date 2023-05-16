fa687bdb9db85872c49e2f52239c04b5f7a50c58
Juanma Romero <xjuanmax@hotmail.com>
2023-05-16 18:07:30 -0400

    sidebar y boton tweet

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,19 @@
+import Button from "./Button"
+
+const NewTweetForm = () => {
+  return (
+    <form className='flex flex-col gap-2 border-b px-4 py-2 '>
+        <div className='flex gap-4'>
+            {/* <ProfileImage src='#url' /> */}
+            <textarea className='flex-grow resize-none overflow-hidden p-4 text-lg outline-none'
+            placeholder='que pasa pa?'
+            />
+        </div>
+        <Button className="">
+            Tweet
+        </Button> 
+    </form>
+  )
+}
+
+export default NewTweetForm
